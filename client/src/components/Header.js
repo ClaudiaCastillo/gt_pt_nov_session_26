4518fef45996175a4f8afe7fec64b35332e3603b
JD <jt7903@gmail.com>
2018-06-01 15:14:15 -0400

    finished search feature

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,6 +4,18 @@ const Header = props => (
   <header className="row split y-center">
     <div className="row y-center">
       <h4><i className="fa fa-folder-open"></i>GiphyBook</h4>
+      {props.isAuth ? (
+        <div className="input-wrap">
+          <input type="text" 
+            name="search" 
+            className="search" 
+            placeholder="Search"
+            onKeyUp={props.getSearchResults}
+            onChange={props.handleChange}
+            value={props.search} />
+          <i className="fa fa-search" onClick={props.getSearchResults}></i>
+        </div>
+      ) : ''}
     </div>
     {props.isAuth ? (
       <div className="row">
