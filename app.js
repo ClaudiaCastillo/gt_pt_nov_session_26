76c833d32e74c52e6d54891cbf8911427dc12b4f
JD <jt7903@gmail.com>
2018-06-12 00:01:49 -0400

    ...

--- app.js
+++ app.js
@@ -3,21 +3,21 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+// Let's create a config file to hold our db username and pass
+const config = require('./config');
 
 const mongoose = require('mongoose');
 
-const local_url = 'mongodb://localhost/gt_pt_26';
-// We need to secure our pass and username
-const db_username = process.env.DB_USERNAME;
-const db_pass = process.env.DB_PASS;
-const mlab_url = `mongodb://${db_username}:${db_pass}@ds117605.mlab.com:17605/test_db`;
-// we need to get the cloud db url
-
-// now we need to setup a variable to see if we're on heroku or not
-// if the NODE_ENV is active, we're on heroku, otherwise it'll be set to 'dev'
+// We need to pull in our cloud database url for production
+// We'll do a check for the environment using a variable
 const env = process.env.NODE_ENV || 'dev';
 
-mongoose.connect(env === 'dev' ? local_url : mlab_url);
+// Let's get the cloud url and save that to a variable
+const mlab_url = `mongodb://${config.db_username}:${config.db_pass}@ds117605.mlab.com:17605/test_db`;
+const db_url = 'mongodb://localhost/gt_pt_26';
+
+// Now we can switch the urls based on if we're in production(on Heroku, etc)
+mongoose.connect(env === 'dev' ? db_url : mlab_url);
 mongoose.Promise = Promise;
 
 const api_routes = require('./routes/api_routes');
